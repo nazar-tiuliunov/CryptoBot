5bdb2c2fb4f8901e85b8f7fa7ac03dae9750baac
Nazar Tiuliunov <tiulinaz@fit.cvut.cz>
2024-05-25 10:58:24 +0200

    Tests added

--- services/binance_api.py
+++ services/binance_api.py
@@ -1,15 +1,17 @@
+import asyncio
 import os
 from binance import AsyncClient, Client
 
 
 async def get_client():
     client = Client(api_key=os.getenv('BINANCE_API_KEY'), api_secret=os.getenv('BINANCE_SECRET_KEY'))
+    print(client)
     return client
 
 
 async def get_account_info():
     client = await get_client()
-    account_info = client.get_account()
+    account_info = await client.get_account()
     return account_info
 
 
@@ -22,4 +24,9 @@ async def get_info_currency(symbol):
     except StopIteration:
         symbol_info = None
     await client.close_connection()
+    print(symbol_info, ticker['lastPrice'])
     return symbol_info, ticker['lastPrice']
+
+
+if __name__ == '__main__':
+    asyncio.run(get_info_currency('BTC'))
